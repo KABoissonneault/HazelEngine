a7af530dc6615a7c297212725477ab456d5a1dfc
Kevin Alexandre Boissonneault <kevin-alexandre.boissonneault@polymtl.ca>
2016-01-12 20:26:26 -0500

    + More tests to check if Min and Max support multiple argument types

--- src/Source/Test/SDK/HE_Math_Test.cpp
+++ src/Source/Test/SDK/HE_Math_Test.cpp
@@ -11,7 +11,8 @@ static_assert(IsPow2(1024), "HE::Math::IsPow2 failed to pass test");
 
 static_assert(Min(2, 3) == 2, "HE::Math::Min failed to pass test");
 static_assert(Min(2, 3, 5, 1) == 1, "HE::Math::Min failed to pass test");
+static_assert(Min(2, 2.4f, 1.0) == 1.0, "HE::Math::Min failed to pass test");
 
 static_assert(Max(2, 3) == 3, "HE::Math::Max failed to pass test");
 static_assert(Max(2, 3, 5, 1) == 5, "HE::Math::Max failed to pass test");
-
+static_assert(Max(2, 2.4f, 1.0) == 2.4f, "HE::Math::Max failed to pass test");
