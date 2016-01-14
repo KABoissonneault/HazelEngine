2dfb2889111e462a6d55e16c9b13d80bba1eea27
Kevin Alexandre Boissonneault <kevin-alexandre.boissonneault@polymtl.ca>
2016-01-13 21:15:35 -0500

    + Forgot to add the new math tests

--- src/Source/Test/SDK/HE_Math_Test.cpp
+++ src/Source/Test/SDK/HE_Math_Test.cpp
@@ -16,3 +16,7 @@ static_assert(Min(2, 2.4f, 1.0) == 1.0, "HE::Math::Min failed to pass test");
 static_assert(Max(2, 3) == 3, "HE::Math::Max failed to pass test");
 static_assert(Max(2, 3, 5, 1) == 5, "HE::Math::Max failed to pass test");
 static_assert(Max(2, 2.4f, 1.0) == 2.4f, "HE::Math::Max failed to pass test");
+
+static_assert(RoundUpToMultipleOf(1, 4) == 4, "HE::Math::RoundUpToMultipleOf failed to pass test");
+static_assert(RoundUpToMultipleOf(13, 9) == 18, "HE::Math::RoundUpToMultipleOf failed to pass test");
+//static_assert(RoundUpToMultipleOf(1, -2) == 4, "HE::Math::RoundUpToMultipleOf failed to pass test"); // Should not compile
