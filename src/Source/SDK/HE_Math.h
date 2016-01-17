3e54b0706f5e2e1b3197aca4a0e09ce0ecdfcefa
Kevin Alexandre Boissonneault <kevin-alexandre.boissonneault@polymtl.ca>
2016-01-16 20:38:22 -0500

    = Less template instatiations, more type inference

--- src/Source/SDK/HE_Math.h
+++ src/Source/SDK/HE_Math.h
@@ -14,33 +14,33 @@ namespace HE
 			return a && !(a & (a - 1));
 		}
 
-		template<class T1, class T2, class T = std::common_type_t<T1, T2>>
-		constexpr T Min(const T1& a, const T2& b) noexcept
+		template<class T1, class T2>
+		constexpr auto Min(const T1& a, const T2& b) noexcept
 		{
 			return a < b ? a : b;
 		}
 
-		template<class T1, class T2, class... Tr, class T = std::common_type_t<T1, T2, Tr...>>
-		constexpr T Min(const T1& a, const T2& b, const Tr&... rest) noexcept
+		template<class T1, class T2, class... Tr>
+		constexpr auto Min(const T1& a, const T2& b, const Tr&... rest) noexcept
 		{
 			return Min(a, Min(b, rest...));
 		}
 
-		template<class T1, class T2, class T = std::common_type_t<T1, T2>>
-		constexpr T Max(const T1& a, const T2& b) noexcept
+		template<class T1, class T2>
+		constexpr auto Max(const T1& a, const T2& b) noexcept
 		{
 			return a > b ? a : b;
 		}
 
-		template<class T1, class T2, class... Tr, class T = std::common_type_t<T1, T2, Tr...>>
-		constexpr T Max(const T1& a, const T2& b, const Tr&... rest) noexcept
+		template<class T1, class T2, class... Tr>
+		constexpr auto Max(const T1& a, const T2& b, const Tr&... rest) noexcept
 		{
 			return Max(a, Max(b, rest...));
 		}
 
 		// Base should be non-zero and unsigned
-		template<class T1, class T2, class T = std::common_type_t<T1, T2>>
-		constexpr T RoundUpToMultipleOf(T1 s, T2 base) noexcept
+		template<class T1, class T2, class Enable = std::enable_if_t<and_<std::is_integral<T1>, std::is_integral<T2>>::value>>
+		constexpr auto RoundUpToMultipleOf(T1 s, T2 base) noexcept
 		{
 			return (s % base) ? s + base - (s % base) : s;
 		}
